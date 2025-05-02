46a8a045625fa133471f5dd28ab2c903796d8143
homanok8 <homanok8@gmail.com>
2025-05-02 11:56:53 +0900

    20250502 main page

--- js/common.js
+++ js/common.js
@@ -66,5 +66,99 @@ $(function() {
         $(this).next().slideToggle();
     });
 
+    // 메인 배너 슬라이드
+    const $bannerItems = $('#bannerList li');
+    const totalBanner = $bannerItems.length;
+    let bannerPage = 0;
+
+    $('#bannerNum em').text(totalBanner); // 배너 개수
+    let slideShow = setInterval(bannerSlide, 5000); // 슬라이드 자동
+
+    function bannerSlide() {
+        $bannerItems.eq(bannerPage).stop().animate({'left': '-100%'});
+        totalBanner - 1 > bannerPage ? bannerPage++ : bannerPage = 0;
+        $bannerItems.eq(bannerPage).stop().css({'left': '100%'});
+        $bannerItems.eq(bannerPage).stop().animate({'left': '0'});
+        $('#bannerNum span').text(`${bannerPage+1}/`);
+    }
+
+    $('#bannerPlayBtn').click(function() {
+        $(this).toggleClass('pause');
+        if ($(this).hasClass('pause')) {
+            clearInterval(slideShow);
+        } else {
+            slideShow = setInterval(bannerSlide, 5000);
+        }
+    });
+
+    // 증명서발급 슬라이드
+    const itemsPerPage = 6;
+    const $slider = $('.clc-crt__list');
+    const $items = $('.clc-crt__item');
+    const $status = $('.clc-crt__btn');
+    const totalItems = $items.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    let currentPage = 0;
+
+    function updateSlider() {
+        const offset = -100 * currentPage;
+        $slider.css('transform', `translateX(${offset}%)`);
+    }
+
+    $('#crtNextBtn').click(function() {
+        if (currentPage < totalPages - 1) {
+            currentPage++;
+            updateSlider();
+            $status.removeClass('active');
+            $status.eq(currentPage).addClass('active');
+        }
+    });
+
+    $('#crtPrevBtn').click(function() {
+        if (currentPage > 0) {
+            currentPage--;
+            updateSlider();
+            $status.removeClass('active');
+            $status.eq(currentPage).addClass('active');
+        }
+    });
+
+    $status.click(function() {
+        $status.removeClass('active');
+        $(this).addClass('active');
+        currentPage = $(this).index();
+        updateSlider();
+    });
+
+    // 생애주기별 서비스
+    const $hubTab = $('.hub-cyc__tabbtn');
+    const $hubBlock = $('.hub-cyc__cntn');
+    
+    $hubTab.click(function() {
+        $hubTab.removeClass('active');
+        $hubBlock.removeClass('active');
+        $(this).addClass('active');
+        $hubBlock.eq($(this).index()).addClass('active');
+    });
+
+    // 탭버튼 공통
+    $('.tab-btn').click(function() {
+        $(this).parents('.tab').find('.tab-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.tab2__btn').click(function() {
+        $(this).parents('.tab2').find('.tab2__btn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.hub-mdm__tabbtn').click(function() {
+        $(this).parents('.hub-mdm__tab').find('.hub-mdm__tabbtn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // 스포츠정보 슬라이드
+    
+
 
 });
